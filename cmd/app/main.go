61128962646887891c3359da7a830d410eac68ed
Oliver Benns <oliverbenns@gmail.com>
2024-05-15 23:20:19 +0100

    x

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -13,7 +13,7 @@ func main() {
 	logger := slog.New(slog.NewJSONHandler(os.Stderr, nil))
 	bybitClient := bybit.NewClient("https://api.bybit.com")
 
-	riskFreeRate := flag.Float64("riskFreeRate", 5.0, "risk free rate")
+	riskFreeRate := flag.Float64("riskFreeRate", 0.05, "risk free rate")
 	flag.Parse()
 
 	svc := app.NewService(logger, bybitClient, *riskFreeRate)
